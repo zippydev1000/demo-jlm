1fe0584938333e055ad66d20195464135d86f23a
zippydev1000 <zippydev100@gmail.com>
2026-08-19 18:28:48 +0300

    simplify DeleteOrder componen

--- DeleteOrder.jsx
+++ DeleteOrder.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 
 const DeleteOrder = () => {
-  return (
-    <div>
-      <h1>Delete Order</h1>
-    </div>
-  );
+  return <h1>Delete Order</h1>;
 };
 
 export default DeleteOrder;
